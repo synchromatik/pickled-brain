49fbbdca02658fd637d842ecb15f5dd8ecb57072
Marko Manojlovic <marko@ivyexec.com>
2019-07-30 13:45:43 +0200

    Updates

---
+++ precache-manifest.7e7ac53f5d893e1c148f4d300bb92fe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0ca2ed60de712219754495e79079cc",
+    "url": "/synchromatik/pickled-brain/index.html"
+  },
+  {
+    "revision": "170c3543d91e8362e629",
+    "url": "/synchromatik/pickled-brain/static/css/main.b2bf753d.chunk.css"
+  },
+  {
+    "revision": "00075039f754573c93ad",
+    "url": "/synchromatik/pickled-brain/static/js/2.1f8ea2f7.chunk.js"
+  },
+  {
+    "revision": "170c3543d91e8362e629",
+    "url": "/synchromatik/pickled-brain/static/js/main.01a4d309.chunk.js"
+  },
+  {
+    "revision": "c3cabd29a13fd18a97e6",
+    "url": "/synchromatik/pickled-brain/static/js/runtime~main.d6874c07.js"
+  }
+]);
